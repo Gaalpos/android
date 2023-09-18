bea3f6f6f2ed7c08dfdd6515f2a2cf0fb2ad1736
Gaalpos <gabripazos13@gmail.com>
2023-09-18 21:46:03 +0200

    09/18/2023 21:45:52

---
+++ BottonNavBar/app/src/main/java/com/example/navbar/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,40 @@
+package com.example.navbar.ui.notifications;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.navbar.databinding.FragmentHomeBinding;
+import com.example.navbar.databinding.FragmentNotificationsBinding;
+import com.example.navbar.ui.home.HomeViewModel;
+
+public class NotifcationsFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
