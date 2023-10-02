00a1365c5e12b8db3b1c07f6865b599b4d62ce3e
gaalpos <gabripazos13@gmail.com>
2023-10-02 12:37:47 +0200

    10/02/2023 12:37:46

---
+++ ActivitySwitcher/app/src/main/java/com/example/activityswitcher/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.activityswitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void onClickClose(View view){
+        finish();
+
+    }
+
+    public void onClickBackActivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
