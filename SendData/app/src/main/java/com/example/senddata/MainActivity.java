c4b383e2b658d2ddec4d6b7a7e9271e9a7467a4c
gaalpos <gabripazos13@gmail.com>
2023-10-03 13:28:48 +0200

    10/03/2023 13:28:46

---
+++ SendData/app/src/main/java/com/example/senddata/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.senddata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.example.senddata.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton;
+    EditText editText;
+    EditText editText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageButton = findViewById(R.id.careto);
+        imageButton.setOnClickListener(view -> onClickSwitch(view));
+    }
+    public void onClickSwitch(View view){
+        editText = (EditText) findViewById(R.id.cajita);
+        String text  = editText.getText().toString();
+        editText2 = findViewById(R.id.cajita2);
+        String text2 = editText2.getText().toString();
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT, text +"-"+text2);
+        intent.putExtra(Intent.EXTRA_TEXT,text);
+        startActivity(intent);
+    }
+
+
+}
