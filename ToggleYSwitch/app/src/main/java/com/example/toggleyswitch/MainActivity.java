154be605f493f21d9aede019d6364bfc4ca514cb
gaalpos <gabripazos13@gmail.com>
2023-10-31 14:27:06 +0100

    10/31/2023 14:27:05

--- ToggleYSwitch/app/src/main/java/com/example/toggleyswitch/MainActivity.java
+++ ToggleYSwitch/app/src/main/java/com/example/toggleyswitch/MainActivity.java
@@ -3,19 +3,42 @@ package com.example.toggleyswitch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     ToggleButton toggleButton;
-    Switch aSwitchh;
+    Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toggleButton=findViewById(R.id.toggleButton);
-        toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> );
+        toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> onCheckedChanged(buttonView, isChecked));
+        aSwitch = findViewById(R.id.switch1);
+        aSwitch.setOnCheckedChangeListener((button, checked) -> manolito((Switch)button, checked));
+    }
+
+    public void manolito(Switch button, boolean checked){
+        ImageView imageView = findViewById(R.id.imageView);
+        if(checked)
+            imageView.setImageResource(R.drawable.avatar_3);
+        else
+            imageView.setImageResource(R.drawable.avatar_4);
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonview, boolean isChecked){
+        ImageView imageView = findViewById(R.id.imageView);
+        if(isChecked)
+            imageView.setImageResource(R.drawable.avatar_1);
+        else
+            imageView.setImageResource(R.drawable.avatar_2);
+
     }
 }
