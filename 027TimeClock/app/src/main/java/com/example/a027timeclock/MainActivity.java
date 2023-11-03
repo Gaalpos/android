2b809841af01ff4dfcf62ebb0d04f596922ed6e3
gaalpos <gabripazos13@gmail.com>
2023-11-03 13:05:35 +0100

    11/03/2023 13:05:34

--- 027TimeClock/app/src/main/java/com/example/a027timeclock/MainActivity.java
+++ 027TimeClock/app/src/main/java/com/example/a027timeclock/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.a027timeclock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textClock = findViewById(R.id.reloj);
         textClock.setOnClickListener(v -> pillarHora());
+        Button button = findViewById(R.id.boton);
+        button.setOnClickListener(view -> tokecito());
+
     }
 
     public void pillarHora(){
         TextView textView = findViewById(R.id.hora);
         textView.setText(textClock.getText().toString());
 
+    }
+
+    public void tokecito(){
+        EditText editText = findViewById(R.id.ponHora);
+        String pon = editText.getText().toString();
+        //Toast.makeText(this, "VER FORMATO DE HORA"+pon, Toast.LENGTH_LONG).show();
+        textClock.setText(pon);
     }
 }
